7f53c8680aa06e13fd5434b0fba863372be7d382
shahnewazz <shahnewaz720@gmail.com>
2025-11-19 23:01:02 +0600

    added changes

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="container mx-auto p-4">
-    <h1 class="text-4xl font-bold text-blue-600">Hello, Tailwind CSS with Vite and Vue!</h1>
+    <h1 class="text-4xl font-bold text-blue-600">Slot</h1>
   </div>
 </template>
 
